4028966d78e2d16e41d36da0c7d23864a1d838f3
ZS-OOD <cs21resch04001@iith.ac.in>
2025-05-28 16:58:45 +0530

    Update extract_features.py

--- extract_features.py
+++ extract_features.py
@@ -13,7 +13,7 @@ import os, json
 
 # Load the model
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model_name = 'ViT-L/14' #   ViT-B/32 # 'ViT-L/14' 'RN-50' ,'RN-101# 
+model_name = 'ViT-B/32' #   ViT-B/32 # 'ViT-L/14' 'RN-50' ,'RN-101# 
 model, preprocess = clip.load(model_name, device)
 
 # CIFAR-10
@@ -35,6 +35,15 @@ with open(out_file, 'wb') as f:
 # get all ood datasets for CIFAR100
 out_datasets = ['SVHN', 'texture', 'LSUN', 'iSUN', 'places365' ]
 
+for dataset in out_datasets:
+    ood_dataset = set_ood_dataset_cifar_10(dataset, preprocess)
+    print(len(ood_dataset))
+    ood_image_features = get_image_features(ood_dataset)
+    ood_out_file = f"outputs/{model_name}_clip_{dataset}_image_feature.pkl"
+    mmcv.mkdir_or_exist(dirname(ood_out_file))
+    with open(ood_out_file, 'wb') as f:
+        pickle.dump(ood_image_features , f)
+            
 for dataset in out_datasets:
     ood_dataset = set_ood_dataset_cifar_100(dataset, preprocess)
     print(len(ood_dataset))
